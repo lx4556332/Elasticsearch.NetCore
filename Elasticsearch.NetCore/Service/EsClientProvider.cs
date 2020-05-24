1b0795c96de46a8516f7aa5896e25eed8142520e
lx4556332 <lx4556332@126.com>
2020-05-24 22:36:18 +0800

    Net Core中使用ES

---
+++ Elasticsearch.NetCore/Service/EsClientProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elasticsearch.NetCore.Service
+{
+    public class EsClientProvider : IEsClientProvider
+    {
+        private readonly IConfiguration _configuration;
+        private ElasticClient _client;
+        public EsClientProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ElasticClient GetClient()
+        {
+            if (_client != null)
+                return _client;
+
+            InitClient();
+            return _client;
+        }
+
+        private void InitClient()
+        {
+            var url = _configuration["elasticsearch:url"];
+            var defaultIndex = _configuration["elasticsearch:index"];
+
+            var node = new Uri(url);
+            _client = new ElasticClient(new ConnectionSettings(node).DefaultIndex(defaultIndex));
+        }
+
+
+    }
+}
---
+++ Elasticsearch.NetCore/Service/IEsClientProvider.cs
@@ -0,0 +1,13 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elasticsearch.NetCore.Service
+{
+    public interface IEsClientProvider
+    {
+        ElasticClient GetClient();
+    }
+}
